30371944ab6aa3d808208080a0c686b198519174
lawalkeyd <lawalkeyd@gmail.com>
2021-07-14 11:12:27 +0100

    feat: Added Course Items, Admin Settings

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Subject, Course, Module, Text, Image, Video
 
 # Register your models here.
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['title', 'created']
+    search_fields = ['title', 'overview']
+
+    prepopulated_fields = {'slug': ('title',)}    
+    inlines = [ModuleInline]
+
+admin.site.register(Text)
+admin.site.register(Image)
+admin.site.register(Video)
--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
-class Subjects(models.Model):
+class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
@@ -15,7 +17,7 @@ class Subjects(models.Model):
 
 class Course(models.Model):
     owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subjects, related_name='courses', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200, unique=True)
     overview = models.TextField()
@@ -36,5 +38,34 @@ class Module(models.Model):
     def __str__(self):
         return self.title
 
-        
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'models__in': ('text', 'image', 'file', 'video')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey()
 
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title    
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()    
